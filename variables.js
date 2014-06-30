553fdfa094839f146ce8dfe74fba86de00ee7e66
Josh Cornelius <joshcornelius9@gmail.com>
2014-06-30 12:21:59 -0400

    completed variables exercise

--- variables.js
+++ variables.js
@@ -7,12 +7,14 @@
 // Store the following into variables: number of children, partner's name, geographic location, job title.
 // Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 
-var children = prompt("How many children?")
-var partnerName = prompt("What would you like your partner's name to be?")
-var geographicLocation = prompt("Where do you want to live?")
-var jobTitle = prompt("What do you want to do?")
+(function(){
+		var children = prompt("How many children?");
+		var partnerName = prompt("What would you like your partner's name to be?");
+		var geographicLocation = prompt("Where do you want to live?");
+		var jobTitle = prompt("What do you want to do?");
 
-alert("You will be a " + jobTitle + " in " + geographicLocation + ", and married to " + partnerName + " with " + children + " kids")
+		alert("You will be a " + jobTitle + " in " + geographicLocation + ", and married to " + partnerName + " with " + children + " kids");
+})();
 
 // EXERCISE: The Age Calculator
 
@@ -23,6 +25,15 @@ alert("You will be a " + jobTitle + " in " + geographicLocation + ", and married
 // Calculate their 2 possible ages based on the stored values.
 // Output them to the screen like so: "They are either NN or NN", substituting the values.
 
+function age(birthyear){
+	var currentYear = 2014;
+	var firstAge = 2014 - birthyear;
+	var secondAge = firstAge - 1;
+
+	console.log('They are either ' + firstAge + " or " + secondAge );
+}
+
+age(1956);
 
 
 // EXERCISE: The Lifetime Supply Calculator
@@ -35,6 +46,16 @@ alert("You will be a " + jobTitle + " in " + geographicLocation + ", and married
 // Calculate how many you would eat total for the rest of your life.
 // Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
 
+function lifetimeSupply(){
+	var currentAge = prompt("What is your current age");
+	var maxAge = prompt("How old are you going to live to?");
+	var dailyConsumption = prompt("How much do you consume per day?");
+	var supply = (maxAge - currentAge) * 365 * dailyConsumption;
+
+	console.log(supply);
+}
+
+lifetimeSupply();
 
 // EXERCISE: The Geometrizer
 
@@ -44,7 +65,15 @@ alert("You will be a " + jobTitle + " in " + geographicLocation + ", and married
 // Calculate the circumference based on the radius, and output "The circumference is NN".
 // Calculate the area based on the radius, and output "The area is NN".
 
+function circleCalc(radius){
+	var circumference = 2 * Math.PI * radius;
+	var area = Math.PI * Math.pow(radius, 2);
 
+	console.log("circumference: " + circumference);
+	console.log("area: " + area);
+}
+
+circleCalc(5);
 
 
 
@@ -56,3 +85,22 @@ alert("You will be a " + jobTitle + " in " + geographicLocation + ", and married
 // Convert it to fahrenheit and output "NN°C is NN°F".
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
+
+function toCelsius(fahrenheit){
+	var celsius = (fahrenheit - 32) * (5/9);
+
+	console.log(fahrenheit + "F is " + celsius + "C");
+}
+
+function toFahrenheit(celsius){
+	var fahrenheit = celsius * (9/5) + 32;
+
+	console.log(celsius + "C is " + fahrenheit + 'F')
+}
+
+toCelsius(55);
+toFahrenheit(25);
+
+
+
+
